b47c1692470afecca65e2e7f77feab62b663e991
Adarsh Ponaka <adarshponaka@gmail.com>
2018-11-04 14:51:04 -0500

    arm tester class created for testing the arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armtester.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by adars on 11/4/2018.
+ */
+
+
+@Autonomous(name = "armTester")
+
+
+public class armtester extends LinearOpMode {
+
+    private DcMotor motorT; //motor top
+    private DcMotor motorB; //motor base
+
+    public void runOpMode(){
+
+    }
+
+
+}
