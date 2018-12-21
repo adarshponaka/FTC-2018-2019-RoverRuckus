79063bbb328278d9e9f3764f40fa63cb344d40e1
Adarsh Ponaka <adarshponaka@gmail.com>
2018-12-21 18:47:25 -0500

    nkb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armtester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armtester.java
@@ -9,7 +9,7 @@ package org.firstinspires.ftc.teamcode;
 /**
  * Created by adars on 11/4/2018.
  */
-
+//pavan
 
 @TeleOp(name="armtester", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
 
